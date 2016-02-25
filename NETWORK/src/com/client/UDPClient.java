5e665769dfe93079909d8f5fd6bc1676ccb95ad6
fruit <neilromblon@live.com>
2016-02-26 01:56:57 +0800

    Update UDPClient.java

--- NETWORK/src/com/client/UDPClient.java
+++ NETWORK/src/com/client/UDPClient.java
@@ -15,9 +15,11 @@ public class UDPClient {
 	public String sentence;
 	public DatagramSocket clientSocket;       
 	public InetAddress IPAddress;
-	public String fileName;
+	private String fileName;
+	private ClientFrame frame;
 	
-	public UDPClient() throws Exception{
+	public UDPClient(ClientFrame frame) throws Exception{
+		this.frame=frame;
 		this.sendData = new byte[1024];
 		this.receiveData = new byte[1024];
 		this.clientSocket = new DatagramSocket();
@@ -34,12 +36,37 @@ public class UDPClient {
 	public void receive() throws Exception {
 		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);       
 		clientSocket.receive(receivePacket);       
-		fileName = new String(receivePacket.getData());  
+		setFileName(new String(receivePacket.getData()));  
 		System.out.println("FROM SERVER: " + fileName);   
 		this.receiveData = new byte[1024];
 	}
 	
+	public boolean receiveSShow() throws Exception {
+		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);       
+		clientSocket.receive(receivePacket); 
+		String fileName=new String(receivePacket.getData()).trim();
+		String status=fileName.substring(0, 1);
+		fileName=fileName.substring(1);
+		setFileName(fileName);  
+		System.out.println("FROM SERVER: " + fileName);   
+		this.receiveData = new byte[1024];
+		if(status.equals("O"))
+			return false;
+		else
+			return true;
+	}
+	
 	public void close() {
 		clientSocket.close();
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+		frame.fileName.setText("FILE NAME: "+getFileName());
+		frame.repaint();
 	}
 }
