c964d7bc7a0567620ff47064507df8ed865e2405
fruit <neilromblon@live.com>
2016-02-25 21:25:03 +0800

    Update UDPServer.java
    lines 33 & 34 - changed capitalized sentence to filename of the picture

--- NETWORK/src/com/server/UDPServer.java
+++ NETWORK/src/com/server/UDPServer.java
@@ -30,8 +30,8 @@ public class UDPServer {
 		this.initCommand(sentence);
 		InetAddress IPAddress = receivePacket.getAddress();                   
 		int port = receivePacket.getPort();                   
-		String capitalizedSentence = sentence.toUpperCase();                   
-		sendData = capitalizedSentence.getBytes();                   
+		String filename = frame.images.get(frame.imageIndex).toString();                   
+		sendData = filename.getBytes();                     
 		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);                   		
 		serverSocket.send(sendPacket);      
 	}
