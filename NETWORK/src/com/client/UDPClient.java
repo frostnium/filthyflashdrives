20dac3b097dde97969c7b2ea839e68be889e21c1
luis-madrigal <lmadrigal.111@gmail.com>
2016-02-26 22:05:54 +0800

    Update UDPClient.java

--- NETWORK/src/com/client/UDPClient.java
+++ NETWORK/src/com/client/UDPClient.java
@@ -40,22 +40,6 @@ public class UDPClient {
 		System.out.println("FROM SERVER: " + fileName);   
 		this.receiveData = new byte[1024];
 	}
-	
-	public boolean receiveSShow() throws Exception {
-		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);       
-		clientSocket.receive(receivePacket); 
-		String fileName=new String(receivePacket.getData()).trim();
-		String status=fileName.substring(0, 1);
-		fileName=fileName.substring(1);
-		setFileName(fileName);  
-		System.out.println("FROM SERVER: " + fileName);   
-		this.receiveData = new byte[1024];
-		if(status.equals("O"))
-			return false;
-		else
-			return true;
-	}
-	
 	public void close() {
 		clientSocket.close();
 	}
@@ -66,7 +50,5 @@ public class UDPClient {
 
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
-		frame.fileName.setText("FILE NAME: "+getFileName());
-		frame.repaint();
 	}
 }
