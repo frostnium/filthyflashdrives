570b82586f660b60046feec61a5304283c9fc506
Neil Romblon <neilromblon@live.com>
2016-02-26 23:24:23 +0800

    Update SlideShow.java
    now has UDPServer property, also calls server's sendData() as it loops thru

--- NETWORK/src/com/mp1/SlideShow.java
+++ NETWORK/src/com/mp1/SlideShow.java
@@ -2,13 +2,11 @@ package com.mp1;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.io.IOException;
 import javax.swing.Timer;
 
 import com.server.ServerFrame;
+import com.server.UDPServer;
 
 public class SlideShow implements ActionListener{
 	
@@ -16,9 +14,11 @@ public class SlideShow implements ActionListener{
 	public Timer timer;
 	public int interval;
 	public boolean isActive;
+	private UDPServer server;
 	
-	public SlideShow(ServerFrame frame, int interval) {
+	public SlideShow(ServerFrame frame, int interval,UDPServer server) {
 		this.frame = frame;
+		this.server=server;
 		this.timer = new Timer(interval, this);
 		timer.start();
 		this.isActive = true;
@@ -29,11 +29,17 @@ public class SlideShow implements ActionListener{
 		// TODO Auto-generated method stub
 		if(frame.imageIndex < frame.images.size()-1) {
 			frame.nextImage();
+			try {
+				server.sendData();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		else {
+			
 			timer.stop();
 			this.isActive = false;
 		}
 	}
-
 }
