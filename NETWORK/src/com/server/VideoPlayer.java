c88f2c6b85d590a2f44980c261ad17dd52466d71
luis-madrigal <lmadrigal.111@gmail.com>
2016-02-28 22:09:05 +0800

    Create VideoPlayer.java

---
+++ NETWORK/src/com/server/VideoPlayer.java
@@ -0,0 +1,23 @@
+package com.server;
+
+public class VideoPlayer extends ServerMedia{
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void prev() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayIndex(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
