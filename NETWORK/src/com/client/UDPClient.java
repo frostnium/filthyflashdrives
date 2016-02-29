d95f23ee5919e7c79b30aaa192199d2ebb16f4ee
luis-madrigal <lmadrigal.111@gmail.com>
2016-03-01 02:13:38 +0800

    added new commands

--- NETWORK/src/com/client/UDPClient.java
+++ NETWORK/src/com/client/UDPClient.java
@@ -7,6 +7,10 @@ public class UDPClient {
 	public static final String PREV = "prev";
 	public static final String EXIT = "exit";
 	public static final String SSHOW = "slideshow";
+	public static final String IMODE = "imode";
+	public static final String VMODE = "vmode";
+	public static final String PLAY = "play";
+	public static final String PAUSE = "pause";
 
 	
 	public byte[] sendData;
