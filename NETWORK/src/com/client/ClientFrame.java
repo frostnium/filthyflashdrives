b8be529945ab466230bbe7aa2acb525da35ac7b1
Neil Romblon <neilromblon@live.com>
2016-03-07 21:02:44 +0800

    Update ClientFrame.java

--- NETWORK/src/com/client/ClientFrame.java
+++ NETWORK/src/com/client/ClientFrame.java
@@ -8,6 +8,7 @@ import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
@@ -15,7 +16,9 @@ import java.net.UnknownHostException;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
+import com.mp1.CustomFilter;
 import com.mp1.Global;
 
 public class ClientFrame extends JFrame implements ActionListener, PropertyChangeListener{
@@ -52,6 +55,12 @@ public class ClientFrame extends JFrame implements ActionListener, PropertyChang
 	private JButton connect;
 	
 	
+	//file uploading components
+	private JButton browsebtn;
+	private JLabel fileUploadName;
+	private JButton uploadbtn;
+	private File uploadFile;
+	
 	private int nChunks;
 	private SwingWorker<Void, Void> worker;
 	
@@ -59,6 +68,10 @@ public class ClientFrame extends JFrame implements ActionListener, PropertyChang
 		this.nChunks = 0;
 		
 		this.client = new UDPClient();
+		
+		this.browsebtn = new JButton();
+		this.fileUploadName = new JLabel();
+		this.uploadbtn = new JButton();
 		
 		this.next = new JButton();
 		this.prev = new JButton();
@@ -78,7 +91,7 @@ public class ClientFrame extends JFrame implements ActionListener, PropertyChang
 		this.playStop = new JButton();
 		this.playStopIcons=new ImageIcon[2];
 		this.playStopIcons[0]=new ImageIcon("icons/play.png");
-		this.playStopIcons[1]=new ImageIcon("icons/pause.png");
+		this.playStopIcons[1]=new ImageIcon("icons/stop.png");
 		
 		this.ipLabel = new JLabel();
 		this.ipTextArea = new JTextArea();
@@ -94,19 +107,19 @@ public class ClientFrame extends JFrame implements ActionListener, PropertyChang
 		this.setBounds(0, 0, 400, 500);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		fileName.setBounds(100, 75, 200, 20);
+				
+		fileName.setBounds(100, 25, 200, 20);
 		fileName.setText("FILE NAME: ");
 		this.getContentPane().add(fileName);
 		
-		ipLabel.setBounds(31, 100, 140, 10);
+		ipLabel.setBounds(31, 50, 140, 10);
 		ipLabel.setText("Enter Server IP Address:");
 		this.getContentPane().add(ipLabel);
 		
-		ipTextArea.setBounds(176, 98, 100, 20);
+		ipTextArea.setBounds(176, 48, 100, 20);
 		this.getContentPane().add(ipTextArea);
 		
-		connect.setBounds(281, 96, 70, 25);
+		connect.setBounds(281, 46, 70, 25);
 		connect.setText("connect");
 		connect.setFont(new Font("Lucida Grande",Font.BOLD,9));
 		connect.setActionCommand("connect");
@@ -114,53 +127,68 @@ public class ClientFrame extends JFrame implements ActionListener, PropertyChang
 		this.getContentPane().add(connect);
 		
 		
-		fileType[0].setBounds(101, 125, 60, 15);
+		fileType[0].setBounds(101, 75, 60, 15);
 		fileType[0].setActionCommand("imagetype");
 		fileType[0].addActionListener(this);
 		fileTypeGroup.add(fileType[0]);
 		this.getContentPane().add(fileType[0]);
 		
-		fileType[1].setBounds(191, 125, 100, 15);
+		fileType[1].setBounds(191, 75, 100, 15);
 		fileType[1].setActionCommand("vatype");
 		fileType[1].addActionListener(this);
 		fileTypeGroup.add(fileType[1]);
 		this.getContentPane().add(fileType[1]);
 		
-		next.setBounds(140, 150, 100, 50);
+		next.setBounds(140, 100, 100, 50);
 		next.setText("NEXT");
 		next.setActionCommand("next");
 		next.addActionListener(this);
 		this.getContentPane().add(next);
 		
-		prev.setBounds(40, 150, 100, 50);
+		prev.setBounds(40, 100, 100, 50);
 		prev.setText("PREVIOUS");
 		prev.setActionCommand("prev");
 		prev.addActionListener(this);
 		this.getContentPane().add(prev);
 		
-		exit.setBounds(240, 150, 100, 50);
+		exit.setBounds(240, 100, 100, 50);
 		exit.setText("EXIT");
 		exit.setActionCommand("exit");
 		exit.addActionListener(this);
 		this.getContentPane().add(exit);
 		
-		slideshow.setBounds(40, 210, 20, 20);
+		slideshow.setBounds(40, 160, 20, 20);
 		slideshow.setIcon(new ImageIcon("icons/slideshow.png"));
 		slideshow.setActionCommand("slideshow");
 		slideshow.addActionListener(this);
 		this.getContentPane().add(slideshow);
 		
-		ssInterval.setBounds(70, 210, 100, 20);
+		ssInterval.setBounds(70, 160, 100, 20);
 		ssInterval.setText("1000");
 		this.getContentPane().add(ssInterval);
 
-		playStop.setBounds(170,210,40,40);
-		playStop.setContentAreaFilled(false);
-		playStop.setBorderPainted(false);
+		playStop.setBounds(170,160,40,40);
 		playStop.setActionCommand("play/stop");
 		playStop.addActionListener(this);
 		playStop.setVisible(false);
 		this.getContentPane().add(playStop);
+		
+		browsebtn.setBounds(40, 190, 80, 30);
+		browsebtn.setText("Browse");
+		browsebtn.setActionCommand("browse");
+		browsebtn.addActionListener(this);
+		this.getContentPane().add(browsebtn);
+		
+		fileUploadName.setBounds(130,190,120,30);
+		fileUploadName.setFont(new Font("Serif",Font.PLAIN,10));
+		fileUploadName.setText("NONE");
+		this.getContentPane().add(fileUploadName);
+		
+		uploadbtn.setBounds(260, 190, 80, 30);
+		uploadbtn.setText("Upload");
+		uploadbtn.setActionCommand("upload");
+		uploadbtn.addActionListener(this);
+		this.getContentPane().add(uploadbtn);
 		
 		image.setBounds(0, 250, 400, 200);
 		this.getContentPane().add(image);
@@ -211,6 +239,24 @@ public class ClientFrame extends JFrame implements ActionListener, PropertyChang
 	
 	public void actionPerformed(ActionEvent arg0) {
 		switch(arg0.getActionCommand()){
+			case "browse":  JFileChooser chooser = new JFileChooser();
+		            		chooser.setCurrentDirectory(new java.io.File("."));
+		            		chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+				            chooser.setDialogTitle("Choose file to upload");
+				            chooser.setFileFilter(new CustomFilter());
+				            chooser.setAcceptAllFileFilterUsed(false);
+				
+				            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+				            	uploadFile=chooser.getSelectedFile();
+				                fileUploadName.setText(uploadFile.getName());
+				                System.out.println("File Name: "+ uploadFile.getName());
+				            } else {
+				            	fileUploadName.setText("NONE");
+				                System.out.println("No Selection ");
+				            }
+							break;
+			case "upload": //TODO: file uplaoding
+							break;
 			case "imagetype": slideshow.setVisible(true);  //TODO: send ping shit
 							  ssInterval.setVisible(true);
 							  playStop.setVisible(false);
