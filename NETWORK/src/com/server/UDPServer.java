c1272dd180ecc3bc4e887f9dcb203502d996ecef
Neil Romblon <neilromblon@live.com>
2016-03-24 20:17:53 +0800

    Update UDPServer.java

--- NETWORK/src/com/server/UDPServer.java
+++ NETWORK/src/com/server/UDPServer.java
@@ -1,17 +1,21 @@
 package com.server;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import javax.imageio.ImageIO;
+import javax.swing.SwingWorker;
 
 import com.mp1.FileType;
 import com.mp1.Global;
@@ -26,18 +30,21 @@ public class UDPServer {
 	public ArrayList<ServerMedia> media;
 	public DatagramSocket serverSocket;
 	public byte[] receiveData;
-	public byte[] sendData;
-	
-	public byte[] sendImageData;
+
 	public byte[] receiveImageData;
-	private byte[] tempImageData;
+	byte[] tempImageData;
 	
-	private int port;
-	private InetAddress tempIP;
+	int port;
+	InetAddress tempIP;
 	
-	private int mediaMode;
-	private String receivedFileName;
-	private boolean imageReceivingMode;
+	int mediaMode;
+	String receivedFileName;
+	boolean imageReceivingMode;
+	
+	private int seqNum;
+	private int ackNum;
+	
+	private SwingWorker<Void, Void> worker;
 	
 	public UDPServer() throws Exception{
 		this.imageReceivingMode = false;
@@ -46,63 +53,34 @@ public class UDPServer {
 		this.media.add(new VideoPlayer());
 		this.serverSocket = new DatagramSocket(9999);
 		this.receiveData = new byte[1024];
-		this.sendData = new byte[1024];
 		this.receiveImageData = new byte[1500];
-		this.sendImageData = new byte[1500];
 		this.tempImageData = new byte[0];
+		this.worker=new ServerSwingWorker(this);
 		this.mediaMode = UDPServer.IMAGE_MODE;
-		while(true) {
-			this.receiveData = new byte[1500];
-			this.sendData = new byte[1500];
-			this.receiveImageData = new byte[1512];
-			this.sendImageData = new byte[1512];
-			this.receive();
-		}
+		
+		this.seqNum=0;
+		this.ackNum=0;
+		
+		this.worker.execute();
+		
 	}
 	
-	
-	public void receive() throws Exception{
-		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);                   			
-		serverSocket.receive(receivePacket);                   
-		String sentence = new String( receivePacket.getData()).trim();                   
-		tempIP = receivePacket.getAddress();                   
-		port = receivePacket.getPort(); 
-		if(imageReceivingMode) {
-			if(sentence.substring(0, 10).equals("TRCOMPLETE")) {
-				System.out.println("COMPLETEE");
-				imageReceivingMode = false;
-				InputStream in = new ByteArrayInputStream(tempImageData);
-				BufferedImage bImageFromConvert = null;
-				receivedFileName = sentence.substring(10);
-				try {
-					bImageFromConvert = ImageIO.read(in);
-					File outputfile = new File("images/"+receivedFileName);
-					ImageIO.write(bImageFromConvert, FileType.getExtension(receivedFileName), outputfile);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				ImageViewer iViewer = (ImageViewer) media.get(mediaMode);
-				iViewer.refreshImageList();
-			}
-			else
-				this.receiveImageData(receivePacket);
-		}
-		else		
-			this.initCommand(sentence);
+	public void sendData(byte[] data) throws IOException{
+		data = Global.concat(ByteBuffer.allocate(4).putInt(ackNum).array(), data);
+		data = Global.concat(ByteBuffer.allocate(4).putInt(seqNum).array(), data);
+		DatagramPacket sendPacket = new DatagramPacket(data, data.length, tempIP,port);
+		serverSocket.send(sendPacket);
 	}
 	
-	public void sendData() throws IOException{
+	public void sendFileName() throws IOException{
 		File file = media.get(mediaMode).mediaFiles[media.get(mediaMode).mediaIndex];
 		String filename = "FILENAME:"+file.getName();
 		System.out.println(filename+tempIP+port);
-		sendData = filename.getBytes();                   
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, tempIP, port);                   		
-		serverSocket.send(sendPacket); 
+		byte[] sendData = filename.getBytes(); 
+		sendData(sendData);
 	}
 	
-	public void sendImage() throws IOException {
+	public void sendImage() throws IOException{
 		if(mediaMode == VIDEO_MODE) 
 			return;
 		File imageFile = media.get(mediaMode).mediaFiles[media.get(mediaMode).mediaIndex];
@@ -110,33 +88,26 @@ public class UDPServer {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();        
 		ImageIO.write(img, FileType.getExtension(imageFile.getName()), baos);
 		baos.flush();
-		byte[] bytes = baos.toByteArray();
-		int interval = 0;
-		int addend = 1500;
-		DatagramPacket sendPacket;
-		
-		int chunksReceived = 0;
-		int seqNum = 0;
-		int ackNum = 0;
-		System.out.println("IMAGE LENgTH: "+bytes.length);
+		byte[] imgBytes = baos.toByteArray();
+		int interval = 0,addend = 1500;
+		int chunksReceived=0;
 		do {
-			sendImageData = new byte[0];
+			byte[] sendImageData = new byte[0];
+			/*
 			sendImageData = Global.concat(sendImageData, ByteBuffer.allocate(4).putInt(ackNum).array());
 			sendImageData = Global.concat(sendImageData, ByteBuffer.allocate(4).putInt(seqNum).array());
-			byte[] dataChunk = Arrays.copyOfRange(bytes, interval, interval+addend);
+			*/
+			byte[] dataChunk = Arrays.copyOfRange(imgBytes, interval, interval+addend);
 			sendImageData = Global.concat(sendImageData, ByteBuffer.allocate(4).putInt(dataChunk.length).array());
 			sendImageData = Global.concat(sendImageData, dataChunk);
-			sendPacket = new DatagramPacket(sendImageData, sendImageData.length, tempIP, port);
-			serverSocket.send(sendPacket);
+			sendData(sendImageData);
 			System.out.println("SENT!! "+sendImageData.length);
-			seqNum++;
-			ackNum++;
 			chunksReceived++;
-			if(interval + addend > bytes.length) 
-				addend = bytes.length - interval;
+			if(interval + addend > imgBytes.length) 
+				addend = imgBytes.length - interval;
 			else
 				interval += addend;
-			if(chunksReceived==Global.nChunksBeforeBuffer){
+			if(chunksReceived==Global.nChunksBeforeAck){
 				try {
 					Thread.sleep(Global.millsToBuffer);
 				} catch (InterruptedException e) {
@@ -144,13 +115,12 @@ public class UDPServer {
 				}
 				chunksReceived=0;
 			}
-		}while(interval < bytes.length);
+		}while(interval < imgBytes.length);
 		
-		System.out.println("DONE");
+		System.out.println("DONE"); //TODO: remove this
 		String message = "TRCOMPLETE";
-		sendData = message.getBytes();                   
-		sendPacket = new DatagramPacket(sendData, sendData.length, tempIP, port);                   		
-		serverSocket.send(sendPacket); 
+		byte[] sendData = message.getBytes();                   
+		sendData(sendData);
 	}
 	
 	public void receiveImageData(DatagramPacket receivePacket) {
@@ -173,7 +143,7 @@ public class UDPServer {
 	
 	public int reqandgetInterval() throws IOException {
 		this.receiveData = new byte[1024];
-		this.sendData = new byte[1024];
+		byte[] sendData = new byte[1024];
 		
 		String message = "RFINT";
 		sendData = message.getBytes();                   
@@ -195,14 +165,14 @@ public class UDPServer {
 			media.get(mediaMode).next();
 			if(mediaMode == UDPServer.IMAGE_MODE)
 				this.stopSlideshow();
-			sendData();
+			sendFileName();
 			sendImage();
 		}
 		else if(command.trim().equals("prev")) {
 			media.get(mediaMode).prev();
 			if(mediaMode == UDPServer.IMAGE_MODE)
 				this.stopSlideshow();
-			sendData();
+			sendFileName();
 			sendImage();
 		}
 		else if(command.trim().equals("slideshow")) {
@@ -212,7 +182,7 @@ public class UDPServer {
 			int interval = this.reqandgetInterval();
 			iViewer.displayIndex(0);
 			iViewer.mediaIndex = 0;
-			this.sendData();
+			sendFileName();
 			iViewer.setSshow(new SlideShow(iViewer,interval,this));
 		}
 		else if(command.trim().equals("imode")) {
@@ -222,7 +192,7 @@ public class UDPServer {
 			this.media.get(mediaMode).setVisible(false);
 			this.mediaMode = UDPServer.IMAGE_MODE;
 			this.media.get(mediaMode).setVisible(true);
-			sendData();
+			sendFileName();
 		}
 		else if(command.trim().equals("vmode")) {
 			if(mediaMode == UDPServer.VIDEO_MODE)
@@ -231,7 +201,7 @@ public class UDPServer {
 			this.media.get(mediaMode).setVisible(false);
 			this.mediaMode = UDPServer.VIDEO_MODE;
 			this.media.get(mediaMode).setVisible(true);
-			sendData();
+			sendFileName();
 		}
 		else if(command.trim().equals("play")) {
 			VideoPlayer vPlayer = (VideoPlayer) media.get(mediaMode);
@@ -250,10 +220,9 @@ public class UDPServer {
 		else if(command.trim().equals("startupload")) {
 			String message = "GOUPLOAD";
 			System.out.println(message);
-			sendData = new byte[1500];
+			byte[] sendData = new byte[1500];
 			sendData = message.getBytes();                   
-			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, tempIP, port);                   		
-			serverSocket.send(sendPacket); 
+			sendData(sendData);                  		
 			this.imageReceivingMode = true;
 		}
 	}
