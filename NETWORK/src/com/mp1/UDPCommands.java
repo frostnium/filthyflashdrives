62d42c0a1b8ef5d6d3c6ac9d850c1a26166f8ab3
Neil Romblon <neilromblon@live.com>
2016-03-26 23:05:02 +0800

    Create UDPCommands.java

---
+++ NETWORK/src/com/mp1/UDPCommands.java
@@ -0,0 +1,13 @@
+package com.mp1;
+
+public class UDPCommands {
+	public static final String NEXT = "next";
+	public static final String PREV = "prev";
+	public static final String EXIT = "exit";
+	public static final String SSHOW = "slideshow";
+	public static final String IMODE = "imode";
+	public static final String VMODE = "vmode";
+	public static final String PLAY = "play";
+	public static final String PAUSE = "pause";
+	public static final String STARTUPLOAD = "startupload";
+}
