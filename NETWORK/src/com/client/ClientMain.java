977dc20ea2089cae80fb108829863e6e0793bbbb
Neil Romblon <neilromblon@live.com>
2016-03-26 23:02:56 +0800

    Update ClientMain.java

--- NETWORK/src/com/client/ClientMain.java
+++ NETWORK/src/com/client/ClientMain.java
@@ -1,13 +1,18 @@
 package com.client;
 
+import java.io.UnsupportedEncodingException;
+
+
+
 public class ClientMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedEncodingException {
 		try {
 			new ClientFrame();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
 
 }
