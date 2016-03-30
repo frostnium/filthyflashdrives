78d942ea49d1fee081dfb531a08fb306a718c41c
Neil Romblon <neilromblon@live.com>
2016-03-31 00:14:40 +0800

    Update Global.java

--- NETWORK/src/com/mp1/Global.java
+++ NETWORK/src/com/mp1/Global.java
@@ -1,30 +1,12 @@
 package com.mp1;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Base64;
-
-import javax.imageio.ImageIO;
-
 public class Global {
-	public static int nChunksBeforeBuffer=1;package com.mp1;
-
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Base64;
-
-import javax.imageio.ImageIO;
-
-public class Global {
-	public static int nChunksBeforeBuffer=1;
-	public static long millsToBuffer=25;
 	public static final long TIMEOUT = 2000;
-	public static final int SERVER_LOSS_PROBABILITY = 60;
-	public static final int CLIENT_LOSS_PROBABILITY = 60;
+	public static final int SERVER_LOSS_PROBABILITY = 50;
+	public static final int CLIENT_LOSS_PROBABILITY = 50;
+	public static final int BUFFERLIMIT = 8;
+	public static final long CLIENT_DELAY = 500;
+	public static final long SERVER_DELAY = 250;
 	
 	
 	public static byte[] concat(byte[] a, byte[] b) {
@@ -36,83 +18,8 @@ public class Global {
 		   return c;
 	}
 	
-    public static BufferedImage decodeToImage(String imageString) {
-
-    	
-        BufferedImage image = null;
-        byte[] imageByte;
-        try {
-            imageByte = Base64.getDecoder().decode(imageString);
-            ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
-            image = ImageIO.read(bis);
-            bis.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return image;
-    }
-    
-    public static String encodeToString(BufferedImage image, String type) {
-        String imageString = null;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-
-        try {
-            ImageIO.write(image, type, bos);
-            byte[] imageBytes = bos.toByteArray();
-    		System.out.println("IMAGE LENgTH: "+imageBytes.length);
-            imageString = Base64.getEncoder().encodeToString(imageBytes);
-
-            bos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return imageString;
-    }
 }
 
-	public static long millsToBuffer=25;
-	public static final long TIMEOUT = 2000;
-	public static final int SERVER_LOSS_PROBABILITY = 10;
-	public static final int CLIENT_LOSS_PROBABILITY = 10;
-	
-	
-	public static byte[] concat(byte[] a, byte[] b) {
-		   int aLen = a.length;
-		   int bLen = b.length;
-		   byte[] c= new byte[aLen+bLen];
-		   System.arraycopy(a, 0, c, 0, aLen);
-		   System.arraycopy(b, 0, c, aLen, bLen);
-		   return c;
-	}
-	
-    public static BufferedImage decodeToImage(String imageString) {
-
-    	
-        BufferedImage image = null;
-        byte[] imageByte;
-        try {
-            imageByte = Base64.getDecoder().decode(imageString);
-            ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
-            image = ImageIO.read(bis);
-            bis.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return image;
-    }
-    
-    public static String encodeToString(BufferedImage image, String type) {
-        String imageString = null;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-
-        try {
-            ImageIO.write(image, type, bos);
-            byte[] imageBytes = bos.toByteArray();
-    		System.out.println("IMAGE LENgTH: "+imageBytes.length);
-            imageString = Base64.getEncoder().encodeToString(imageBytes);
-
-            bos.close();
-        } catch (IOException e) {
             e.printStackTrace();
         }
         return imageString;
